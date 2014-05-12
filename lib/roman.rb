8788b011ce6ba20cb5614e7e020f052f1fc6a6ec
Mikhail Dubov <mdubov@gmail.com>
2014-05-12 15:56:24 +0100

    Refactored code for brevity, deleted extra files

--- lib/roman.rb
+++ lib/roman.rb
@@ -1,18 +1,24 @@
 def roman_number(num)
-  roman_hash = {1 => 'I', 5 => 'V', 10 => 'X', 50 => 'L', 100 => 'C', 500 => 'D', 1000 => 'M'}
-  result = '' # default roman numeral is an empty string 
+  roman_hash = {
+    1 => 'I',
+    4 => 'IV',
+    5 => 'V',
+    9 => 'IX',
+    10 => 'X',
+    40 => 'XL',
+    50 => 'L',
+    90 => 'XC',
+    100 => 'C',
+    400 => 'CD',
+    500 => 'D',
+    900 => 'CM',
+    1000 => 'M'
+  }
+
+  result = '' # default roman numeral is an empty string
   roman_hash.keys.reverse.each do |key|
     result += roman_hash[key] * (num / key)
     num %= key
   end
-  special_case(result)
-end
-
-def special_case(string)
-  special_cases = {'DCCCC' => 'CM', 'CCCC' => 'CD', 'LXXXX' => 'XC',
-                   'XXXX' => 'XL', 'VIIII' => 'IX', 'IIII' => 'IV'}
-  special_cases.each do |key, value|
-    string.gsub!(key, value)
-  end
-  string
+  result
 end
